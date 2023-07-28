8bca301b22ea7b8bd6fb54f6d27c255181202f76
momenur <momenurislam6@gmail.com>
2023-07-28 18:11:07 +0600

    added delete method for customer order

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrpqnh1.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -47,13 +47,22 @@ async function run() {
             const result = await ordersCollection.find(query).toArray();
             res.send(result)
         })
-
+        app.get('/customerOrder', async(req, res) => {
+            const result = await ordersCollection.find().toArray();
+            res.send(result)
+        })
         app.post('/order', async(req, res) => {
             const item = req.body;
-            console.log(item);
             const result = await ordersCollection.insertOne(item);
             res.send(result)
         })
+
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.send(result)
+          })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
